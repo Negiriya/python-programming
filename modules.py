d2914d9e857aaf7c63c4e9c3f9f96e895ce45278
Riya negi <126748885+Negiriya@users.noreply.github.com>
2026-08-10 20:56:15 +0530

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,26 @@
+# Using a Built-in Module
+
+import math
+
+number = 25
+
+print("Square root:", math.sqrt(number))
+print("Power:", math.pow(2, 3))
+
+
+# Using the random module
+
+import random
+
+random_number = random.randint(1, 10)
+
+print("Random number:", random_number)
+
+
+# Using the datetime module
+
+import datetime
+
+today = datetime.date.today()
+
+print("Today's date:", today)
