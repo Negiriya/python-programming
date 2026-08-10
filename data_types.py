4fdb6878d862bf9247fb9e1111e5e5131af18d84
Riya negi <126748885+Negiriya@users.noreply.github.com>
2026-08-10 20:44:59 +0530

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,14 @@
+name = "Riya"
+age = 23
+height = 4.9
+is_student = False
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
